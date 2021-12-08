8104fd5a09d14896a70f8e96ea9ebf9db8b36daa
Nökkvi <nokkvi96@gmail.com>
2021-12-08 16:45:32 +0000

    removed reactStrictMode

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const Dotenv = require("dotenv-webpack");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
   swcMinify: true,
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {

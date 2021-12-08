038c09dd1986c73e93f9eb465e363f79cf8d310d
Nökkvi <nokkvi96@gmail.com>
2021-12-08 16:22:50 +0000

    removing unused dep

--- next.config.js
+++ next.config.js
@@ -3,27 +3,16 @@ const Dotenv = require("dotenv-webpack");
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
+
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Add the new plugin to the existing webpack plugins
     config.plugins.push(new Dotenv({ silent: true }));
 
     return config;
   },
-  env: {
-    NEXT_PUBLIC_GIPHY_API_KEY: process.env.NEXT_PUBLIC_GIPHY_API_KEY,
-  },
   experimental: {
     // ssr and displayName are configured by default
     styledComponents: true,
-  },
-  images: {
-    domains: [
-      "media0.giphy.com",
-      "media1.giphy.com",
-      "media2.giphy.com",
-      "media3.giphy.com",
-      "media4.giphy.com",
-    ],
-    formats: ["image/avif", "image/webp"],
   },
 };

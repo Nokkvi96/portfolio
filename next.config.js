304b76f507a85a5aedc713c26d745634944a01d3
Nökkvi <nokkvi96@gmail.com>
2021-12-09 11:01:14 +0000

    removed experimental

--- next.config.js
+++ next.config.js
@@ -3,15 +3,12 @@ const Dotenv = require("dotenv-webpack");
 /** @type {import('next').NextConfig} */
 module.exports = {
   swcMinify: true,
+  styledComponents: true,
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Add the new plugin to the existing webpack plugins
     config.plugins.push(new Dotenv({ silent: true }));
 
     return config;
-  },
-  experimental: {
-    // ssr and displayName are configured by default
-    styledComponents: true,
   },
 };

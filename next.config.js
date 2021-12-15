76d635713403ec5c66e5db94824fc08d44844dd1
Nökkvi <nokkvi96@gmail.com>
2021-12-15 12:24:43 +0000

    babel to swc

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const Dotenv = require("dotenv-webpack");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // swcMinify: true,
-  // styledComponents: true,
+  swcMinify: true,
+  styledComponents: true,
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Add the new plugin to the existing webpack plugins

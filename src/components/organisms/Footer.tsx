b5f6577acdf7a28ed21f0e528578a3c1daea6753
Nökkvi <nokkvi96@gmail.com>
2022-02-02 20:25:05 +0000

    footer added

---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,9 @@
+import { Box, Contain, Flex } from "@components/system";
+
+export const Footer: React.FC = ({ children }) => {
+  return (
+    <Flex flexDirection="column">
+      {/* <Contain maxWidth={1440}></Contain> */}
+    </Flex>
+  );
+};

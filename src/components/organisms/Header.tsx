d8f44a6087346ff1986816e4123915a3cf854672
Nökkvi <nokkvi96@gmail.com>
2022-02-02 20:23:17 +0000

    lint

---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,9 @@
+import { Box, Contain, Flex } from "@components/system";
+
+export const Header: React.FC = ({ children }) => {
+  return (
+    <Flex flexDirection="column">
+      {/* <Contain maxWidth={1440}></Contain> */}
+    </Flex>
+  );
+};
--- src/components/templates/BaseLayout.tsx
+++ src/components/templates/BaseLayout.tsx
@@ -1,8 +1,9 @@
 import { Box, Contain, Flex } from "@components/system";
-
+import { Header } from "@components/organisms";
 export const BaseLayout: React.FC = ({ children }) => {
   return (
     <Flex flexDirection="column" height="100vh">
+      <Header />
       <Box as="main" flex={"1 0 auto"} display="block" bg="bg">
         <Contain>{children}</Contain>
       </Box>
